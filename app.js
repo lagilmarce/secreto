a8d3d380566a512446574f64eb18881b8eee842e
Marcela <lagil.marce@gmail.com>
2020-11-12 15:33:54 -0500

    incial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res)=> res.send('<h1>HcUy6Re2LLBRtj</h1>'))
+
+app.listen(3000,()=> console.log('listening in port 3000'))
